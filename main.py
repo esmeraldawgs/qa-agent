e6ffb3bac2436fd2f165fcd6d6c334a5d14e9da6
ENSEVAL\esmeralda.wangsa <esmeraldawangsa@gmail.com>
2026-05-04 16:00:23 +0700

    fix: add port config for railway

--- main.py
+++ main.py
@@ -66,5 +66,5 @@ def home():
 
 # 🔥 INI YANG PENTING BUAT RAILWAY
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
